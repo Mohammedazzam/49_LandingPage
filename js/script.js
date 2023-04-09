e5aa3b2857b8bfcccb0a616d337e8d70fd4f9fc0
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2023-04-09 22:58:49 +0300

    section-02

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
-const linkDrop = document.getElementById('link-lang')
+const linkDrop = document.getElementById("link-lang");
 
-linkDrop.addEventListener('click', function(e){
-    e.preventDefault();
-    document.querySelector('.sub-menu').classList.toggle('show')
-})
+linkDrop.addEventListener("click", function (e) {
+  e.preventDefault();
+  document.querySelector(".sub-menu").classList.toggle("show");
+});
+
+// Swiper
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  loop: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: ".button-next",
+    prevEl: ".button-prev",
+  },
+});
