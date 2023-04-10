fc3ac635e8874455692d7c9d7cd630009df4e793
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2023-04-10 22:16:54 +0300

    section04

--- js/script.js
+++ js/script.js
@@ -27,3 +27,31 @@ const Section03swiper = new Swiper(".section-03 .swiper", {
       prevEl: "section-03 .button-prev",
     },
   });
+
+
+  const Section04swiper = new Swiper(".section-04 .swiper", {
+    // Optional parameters
+    loop: true,
+    // Navigation arrows
+    navigation: {
+      nextEl: "section-04 .button-next",
+      prevEl: "section-04 .button-prev",
+    },
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 2,
+        spaceBetween: 0
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 2,
+        spaceBetween: 0
+      },
+      // when window width is >= 640px
+      640: {
+        slidesPerView: 3,
+        spaceBetween: 0
+      }
+    }
+  });
